24b932434d3218866f58475178cfd078b3523896
Joabe Machado <joabemachadodeabreu@gmail.com>
2020-04-24 17:17:14 -0300

    commit

--- functions/object.js
+++ functions/object.js
@@ -6,10 +6,27 @@ export default obj => {
         getKeyByValue: (value) => {
             return Object.keys(object).find(key => object[key] == value)
         },
-        hasOwnProperty: (dot_path) => {
+        hasOwnProperty: (propertyPathDoted) => {
+            if (!propertyPathDoted)
+                return false;
+
+            var properties = propertyPathDoted.split('.');
+
+            for (var i = 0; i < properties.length; i++) {
+                var prop = properties[i];
+
+                if (!obj || !obj.hasOwnProperty(prop)) {
+                    return false;
+                } else {
+                    obj = obj[prop];
+                }
+            }
+            return true;
+        },
+        get: (dot_path, _default) => {
             var arr = dot_path.split(".");
             while(arr.length && (obj = obj[arr.shift()]));
-            return obj;
+            return obj || _default;
         }
     }
 }
